fb98aa15632aa8af2adaaf39787841a374ce33e1
Dirk Dobson <dirkvancouver@live.com>
2018-09-08 14:17:16 -0600

    comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,26 @@
 class CommentsController < ApplicationController
   def index
+
   end
 
   def show
+    @comment = 
   end
 
   def new
+    @comment = Comment.new
+    render partial: "form"
   end
+
+  def edit
+    render partial: "form"
+  end
+
+  def create 
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      redirect_to
+
+
 end
