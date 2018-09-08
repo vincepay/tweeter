a2de7e8b059aa0c5ea2372565bab4464b92e05d0
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-09-08 13:08:40 -0600

    bio controller

--- app/controllers/bios_controller.rb
+++ app/controllers/bios_controller.rb
@@ -3,11 +3,39 @@ class BiosController < ApplicationController
   end
 
   def show
+    @bios = current_user.bios
+  end
+
+  def create
+    
+    @bio = current_user.bio.new(bio_params)
+    if @bio.save
+      redirect_to user_bio_path
+    else
+      render :new
+    end
+  end
+
+  def udpate
+    if @bio.update(bio_params)
+      redirect_to user_bio_path
+    else
+      render :edit
+    end
   end
 
   def new
+    @bio = Bio.new
+    render partial: "form"
   end
 
   def edit
+    @bio = current_user.bios.find(params[:id])
+    render partial: "form"
   end
+
+  private
+    def bio_params
+      params.require(:bio).permit(:profile_image, :description)
+    end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,43 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
   def index
+    @posts = current_user.posts
   end
 
   def show
   end
 
   def new
+    @post = Post.new
+    render partial: "form"
   end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to current_user.posts
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = current_user.posts.find(params[:id])
+    render partial: "form"
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to user_posts_path(current_user)
+  end
+
+  private
+   def set_post
+    @post = current_user.posts.find(params[:id])
+   end
+
+   def post_params
+     params.require(:post).permit(:content, :user_id)
+   end
+
 end
