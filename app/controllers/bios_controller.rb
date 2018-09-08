25526cef1f264abdb72762442ef29256581b50ab
Vince Payandeh <varghapayandeh@gmail.com>
2018-09-08 11:15:21 -0600

    fixed routes, models, v. preliminary stuff

---
+++ app/controllers/bios_controller.rb
@@ -0,0 +1,13 @@
+class BiosController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,27 @@
 Rails.application.routes.draw do
+  root 'posts#index'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users do
+    resources :posts do
+      scope "posts/:posts_id", as: "posts" do
+        resources :comments, only: [:new, :create]
+      end
+    end
+
+    resources :bios, only:[:new, :create, :show]
+  end
+
+
+=begin
+/users/user_id/
+/users/user_id/posts   <---> ? unnecessary
+
+/users/user_id/posts/post_id/     shows a specific post of the user and its associated comments
+
+/users/user_id/posts/post_id/comment/commet_id
+
+=end
+
+
 end
